2b0f0fa5dde1cc8abf8187f476ca4e782b259b3e
Loki <sneyed@gmail.com>
2021-09-29 18:27:07 +1000

    add feature toggle for random unsplash images

--- nextjs-wp/nextjs/src/components/cover-image.js
+++ nextjs-wp/nextjs/src/components/cover-image.js
@@ -1,24 +1,28 @@
-import { cssJoin } from "../lib/array";
 import Image from "next/image";
 import Link from "next/link";
+import { randomImg } from "../config/featureToggles";
+import { cssJoin } from "../lib/array";
 
 export default function CoverImage({ title, coverImage, slug }) {
+  const src =
+    coverImage?.sourceUrl ||
+    (randomImg && "https://source.unsplash.com/1000x500/?beach,tropical");
+
+  const valid = src;
+
   const image = (
     <Image
       width={2000}
       height={1000}
       alt={`Cover Image for ${title}`}
-      src={
-        coverImage?.sourceUrl ||
-        "https://source.unsplash.com/1000x500/?beach,tropical"
-      }
+      src={src}
       className={cssJoin([
         "shadow-small",
         slug && "hover:shadow-medium transition-shadow duration-200",
       ])}
     />
   );
-  return (
+  return valid ? (
     <div className="sm:mx-0">
       {slug ? (
         <Link href={`/posts/${slug}`}>
@@ -28,5 +32,5 @@ export default function CoverImage({ title, coverImage, slug }) {
         image
       )}
     </div>
-  );
+  ) : null;
 }
---
+++ nextjs-wp/nextjs/src/config/featureToggles.js
@@ -0,0 +1,1 @@
+export const randomImg = true; // If a post does not have a featured image defined in wordpress, use a random image from Unsplash
