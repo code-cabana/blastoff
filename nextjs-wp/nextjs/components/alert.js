948cb3c2e6df929e5894196e60f85a01a8cc7974
Loki <sneyed@gmail.com>
2021-09-29 17:19:38 +1000

    remove classnames dependency

--- nextjs-wp/nextjs/components/alert.js
+++ nextjs-wp/nextjs/components/alert.js
@@ -1,31 +1,33 @@
-import Container from './container'
-import cn from 'classnames'
-import { EXAMPLE_PATH } from '../lib/constants'
+import Container from "./container";
+import { EXAMPLE_PATH } from "../lib/constants";
+import { cssJoin } from "../lib/array";
 
 export default function Alert({ preview }) {
   return (
     <div
-      className={cn('border-b', {
-        'bg-accent-7 border-accent-7 text-white': preview,
-        'bg-accent-1 border-accent-2': !preview,
-      })}
+      className={cssJoin([
+        "border-b",
+        preview
+          ? "bg-accent-7 border-accent-7 text-white"
+          : "bg-accent-1 border-accent-2",
+      ])}
     >
       <Container>
         <div className="py-2 text-center text-sm">
           {preview ? (
             <>
-              This is a page preview.{' '}
+              This is a page preview.{" "}
               <a
                 href="/api/exit-preview"
                 className="underline hover:text-cyan duration-200 transition-colors"
               >
                 Click here
-              </a>{' '}
+              </a>{" "}
               to exit preview mode.
             </>
           ) : (
             <>
-              The source code for this blog is{' '}
+              The source code for this blog is{" "}
               <a
                 href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
                 className="underline hover:text-success duration-200 transition-colors"
@@ -38,5 +40,5 @@ export default function Alert({ preview }) {
         </div>
       </Container>
     </div>
-  )
+  );
 }
---
+++ nextjs-wp/nextjs/lib/array.js
@@ -0,0 +1,1 @@
+export const cssJoin = (classNames) => classNames.filter(Boolean).join(" ");
