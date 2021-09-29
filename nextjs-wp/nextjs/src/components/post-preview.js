5e868ec7af6f35c0581660d91c377dd3555515c5
Loki <sneyed@gmail.com>
2021-09-29 18:23:22 +1000

    post-preview always show cover image

--- nextjs-wp/nextjs/src/components/post-preview.js
+++ nextjs-wp/nextjs/src/components/post-preview.js
@@ -1,7 +1,7 @@
-import Avatar from '../components/avatar'
-import Date from '../components/date'
-import CoverImage from './cover-image'
-import Link from 'next/link'
+import Avatar from "../components/avatar";
+import Date from "../components/date";
+import CoverImage from "./cover-image";
+import Link from "next/link";
 
 export default function PostPreview({
   title,
@@ -14,9 +14,7 @@ export default function PostPreview({
   return (
     <div>
       <div className="mb-5">
-        {coverImage && (
-          <CoverImage title={title} coverImage={coverImage} slug={slug} />
-        )}
+        <CoverImage title={title} coverImage={coverImage} slug={slug} />
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
         <Link href={`/posts/${slug}`}>
@@ -35,5 +33,5 @@ export default function PostPreview({
       />
       <Avatar author={author} />
     </div>
-  )
+  );
 }
